6da38257a54b3e9dae3f5e321dbccd72e165b694
Shourya Bansal <sbansal@reliable.co>
2024-06-07 11:25:55 -0700

    Update Abbreviations and Organization

--- fish/abbreviations/git.fish
+++ fish/abbreviations/git.fish
@@ -1,25 +1,5 @@
 # Git Abbreviations
 
-# Utilities 
-function git_main_branch
-    command git rev-parse --git-dir &>/dev/null; or return
-
-    set -l ref
-    for ref in refs/heads/main refs/heads/trunk refs/heads/mainline refs/heads/default refs/heads/stable refs/heads/master \
-                  refs/remotes/origin/main refs/remotes/origin/trunk refs/remotes/origin/mainline refs/remotes/origin/default refs/remotes/origin/stable refs/remotes/origin/master \
-                  refs/remotes/upstream/main refs/remotes/upstream/trunk refs/remotes/upstream/mainline refs/remotes/upstream/default refs/remotes/upstream/stable refs/remotes/upstream/master
-        if command git show-ref -q --verify $ref
-            echo (basename $ref)
-            return 0
-        end
-    end
-
-    # If no main branch was found, fall back to master but return error
-    echo master
-    return 1
-end
-
-
 abbr g 'git'
 
 # General Git Tools
@@ -71,7 +51,7 @@ abbr gco 'git checkout'
 abbr gcor 'git checkout --recurse-submodules'
 abbr gcb 'git checkout -b'
 abbr gcB 'git checkout -B'
-abbr gcm 'git checkout $(git_main_branch)'
+abbr gcom 'git checkout $(git_main_branch)'
 
 # Cherry Pick
 abbr gcp 'git cherry-pick'
@@ -81,7 +61,7 @@ abbr gcpc 'git cherry-pick --continue'
 # Committing
 abbr gc 'git commit --verbose'
 abbr gcam 'git commit --all --message'
-abbr gcmsg 'git commit --message'
+abbr gcm 'git commit --message'
 abbr gca 'git commit --verbose --all'
 abbr gca! 'git commit --verbose --all --amend'
 abbr gc! 'git commit --verbose --amend'
