61a3007eaaab148dfe883d9d2d57b7fbfce43791
Shourya Bansal <sbansal@reliable.co>
2024-06-19 14:00:00 -0700

    Add eza abbreviations

---
+++ fish/abbreviations/eza.fish
@@ -0,0 +1,9 @@
+abbr ls 'eza'
+abbr lsl 'eza -l'
+abbr lsla 'eza -la'
+abbr lslt 'eza -l --tree --level=2'
+abbr lst 'eza --tree --level=2'
+abbr lsta 'eza --all --tree --level=2'
+abbr lstla 'eza -la --tree --level=2'
+abbr exa 'eza'
+
