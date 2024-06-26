4176462e58938a87fa4315726d7682e8e5cfc90e
Shourya Bansal <sbansal@reliable.co>
2024-06-26 10:16:15 -0700

    Update netrw Remaps

--- nvim/lua/sharu/remap.lua
+++ nvim/lua/sharu/remap.lua
@@ -3,7 +3,8 @@ vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "Jk", "<Esc>")
 vim.keymap.set("i", "JK", "<Esc>")
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Rex)
+vim.keymap.set("n", "<leader>prv", vim.cmd.Rex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
