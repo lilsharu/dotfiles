69dc810a2306918db9aa04fc4dd4e3f6df083ea6
Shourya Bansal <sbansal@reliable.co>
2024-07-24 13:15:19 -0700

    Update clangd lsp config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -47,4 +47,23 @@ cmp.setup({
 
 -- (Optional) Configure lua language server for neovim
 require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+
+-- Configure Clangd
+require("lspconfig").clangd.setup({
+	cmd = {
+		"clangd",
+		"--background-index",
+		"-j=12",
+		"--query-driver=/usr/bin/**/clang-*,/bin/clang,/bin/clang++,/usr/bin/gcc,/usr/bin/g++",
+		"--clang-tidy",
+		"--clang-tidy-checks=*",
+		"--all-scopes-completion",
+		"--cross-file-rename",
+		"--completion-style=detailed",
+		"--header-insertion-decorators",
+		"--header-insertion=iwyu",
+		"--pch-storage=memory",
+	},
+})
+
 lsp.setup()
